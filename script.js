6d49df9d0ed72a7cbcaa4f00a6ebe87b625fa7d5
saqib mearani <saqibmearani@gmail.com>
2025-10-30 23:32:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Welcome to JS");
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS Land...\nHappy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+alert("Happy Coding \nPrevent this page from creating additional dialogs")
+console.log("Hello... I can run JS through my web browser’s console.");
