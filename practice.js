cc1fbee113f9aa03ee0bc3572a301c5109589133
saqib mearani <saqibmearani@gmail.com>
2025-11-07 23:35:12 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,65 @@
+// //chapter # 1
+// alert("saqib")
+// alert("mearani")
+// alert("saqibmearani@gmail.com")
+// alert("03363399131")
+// alert("saqib123456")
+// alert("You're learning JavaScript!")
+// alert("hello what can i help you!")
+// //chapter # 2
+// var firstname = 'Saqib';
+// alert(firstname)
+// var city = "karachi"
+// alert(city)
+// var teamname = "Code Masters";
+// alert(teamname);
+// var bestman = "Charlie";
+// bestman = "Saqib";
+// alert(bestman)
+// //chapter # 3
+// var caseqty = 55;
+// alert(caseqty);
+// var caseqty = 144;
+// alert(caseqty);
+// var num = 9;
+// alert(num)
+// var a =7
+// var b =9
+// alert(a+b)
+// var merchTotal =100
+// var shipingcharges =10
+// var total = merchTotal+shipingcharges
+// alert(total)
+// var c=20
+// var c=25
+// var d=a+a
+// alert(d)
+// //chapter # 4
+// var x = 10, y = 20, z = 30
+// alert(x+y+z)
+// //Legal Variable Names
+// var userName;
+// var _totalAmount;
+// var $price;
+// var age1;
+// var first_name;
+// //illegal Variable Names
+// // var 1name;     
+// // var user-name;  
+// // var var;       
+// // var first name; 
+// // var @total;     
+var r = "letter"
+var s="Number"
+var t =" $"
+var u = "_"
+var v = "$my_1stVariable"
+var w = "$name"
+var x = "_name"
+var y = "name"
+var z = "sensitive"
+var g = "keyword"
+let name = "Saqib";
+alert(`Variable names can only contain ${r}, ${s} ,${t} and ${u} For example ${v}\n
+    Variables must begin with a ${r}, ${s} and ${t} For example ${w}, ${x} ,${y} \n
+    Variable names are case ${z}\nVariable names should not be JS${g}`);
